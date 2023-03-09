a6927a07cf85a3f251c01e7483f3974a69b8d7cc
sebsilas <sebsilas@gmail.com>
2023-03-09 11:02:28 +0100

    Minification of WJD

---
+++ R/WJD.R
@@ -0,0 +1,21 @@
+
+
+
+#' Weimar Jazz Database (WJD), N-gram version (minified)
+#'
+#' A dataset containing the melodies and other attributes of just over 11,000 melodic jazz solo phrases
+#'
+#' @format A function containing different versions of the WJD data frame, returned with "main", "ngram", "phrases" and "files" keys
+#' @source \url{https://jazzomat.hfm-weimar.de/dbformat/dboverview.html}
+"WJD_ngram_minified"
+
+
+#' Weimar Jazz Database (WJD), phrase version
+#'
+#' A dataset containing the melodies and other attributes of just over 11,000 melodic jazz solo phrases
+#'
+#' @format A function containing different versions of the WJD data frame, returned with "main", "ngram", "phrases" and "files" keys
+#' @source \url{https://jazzomat.hfm-weimar.de/dbformat/dboverview.html}
+"WJD_phrase_item_bank"
+
+
