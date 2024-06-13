6f3bbbd6b6868271a4e9aa598d229120bf5c53fb
sebsilas <sebsilas@gmail.com>
2024-06-13 10:20:24 +0000

    Update dict

--- data-raw/PBET_dict.R
+++ data-raw/PBET_dict.R
@@ -8,5 +8,7 @@ PBET_dict <- musicassessr::dict(additional_dict = PBET_dict_df)
 
 PBET_dict_t <- as.data.frame(PBET_dict)
 
+# PBET_dict_t %>% dplyr::filter(key == "arrhythmic_melody_trial_instruction_text_sing_melody_first")
+
 usethis::use_data(PBET_dict, overwrite = TRUE)
 
